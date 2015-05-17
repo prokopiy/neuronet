278070ee8d063d815ccc22d8b43012b5d649db28
prokopiy <prokopiy@inbox.ru>
2015-05-17 03:55:56 +0000

    Синхронизация с остальными

--- src/neuron.erl
+++ src/neuron.erl
@@ -23,8 +23,8 @@ new(Memory_size) ->
   Data = #{
            in_powers => [],
            memory => gen_clean_memory(Memory_size),
-           in_links => [],
-           out_links => [],
+           in_links => #{},
+           out_links => #{},
            error => 0,
            num_active_links => 0
           },
@@ -70,19 +70,25 @@ loop(Data) ->
     {request, Pid, stop} ->
       io:format("Neuron~w stopped~n", [self()]),
       Pid ! {reply, self(), ok};
-    {request, Pid, {set_link_out, Output_neuron_Pid, W}} ->
+    {request, Pid, {set_link_out, Output_neuron_pid, W}} ->
       Current_out_links = maps:get(out_links, Data),
 %       {out_links, Current_out_links} = lists:keyfind(out_links, 1, Data),
-      New_out_links = lists:keystore(Output_neuron_Pid, 1, Current_out_links, {Output_neuron_Pid, W}),
-      NewData = maps:put(out_links, New_out_links, Data),
+%       New_out_links = lists:keystore(Output_neuron_Pid, 1, Current_out_links, {Output_neuron_Pid, W}),
+      New_out_links = Current_out_links#{Output_neuron_pid := W},
+%       New_out_links = maps:put(Output_neuron_pid, W, Current_out_links),
+%       NewData = maps:put(out_links, New_out_links, Data),
+      NewData = Data#{out_links := New_out_links},
 %       NewData = lists:keyreplace(out_links, 1, Data, {out_links, New_out_links}),
       Pid ! {reply, self(), ok},
       loop(NewData);
     {request, Pid, {set_link_in, Input_neuron_pid, W}} ->
       Current_in_links = maps:get(in_links, Data),
 %       {in_links, Current_in_links} = lists:keyfind(in_links, 1, Data),
-      New_in_links = lists:keystore(Input_neuron_pid, 1, Current_in_links, {Input_neuron_pid, W}),
-      NewData = maps:put(in_links, New_in_links, Data),      
+%       New_in_links = lists:keystore(Input_neuron_pid, 1, Current_in_links, {Input_neuron_pid, W}),
+      New_in_links = Current_in_links#{Input_neuron_pid := W},
+%       New_in_links = maps:put(Input_neuron_pid, W, Current_in_links),
+%       NewData = maps:put(in_links, New_in_links, Data),      
+      NewData = Data#{in_links := New_in_links},
 %       NewData = lists:keyreplace(in_links, 1, Data, {in_links, New_in_links}),
       Pid ! {reply, self(), ok},
       loop(NewData);
